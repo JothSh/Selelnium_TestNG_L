f15f5a114f08636b6d2c9d0aa38545f882a60ce2
JothSh <jotheeshit@gmail.com>
2024-10-18 10:24:47 +0530

    Updated

---
+++ SeleniumLearnings_MaveTestNG/src/test/java/testNG/ParameterizationExample.java
@@ -0,0 +1,13 @@
+package testNG;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ParameterizationExample {
+	
+	@Test
+	
+public void myName() {
+	System.out.println("Myname is ");
+}
+}
